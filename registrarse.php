eed0df94a2867508a5deb73aeec683601871cc2d
jorgec87 <jorgec87@gmail.com>
2017-05-27 17:25:31 -0400

    actualizacion  formulario registrarse
    J.Carrenca

--- registrarse.php
+++ registrarse.php
@@ -49,25 +49,25 @@
                             
                             <div class="form-group row">
                                 <label for="inputEmail3" class="col-sm-2 col-form-label">Correo</label>
-                                <div class="col-sm-10">
+                                <div class="col-sm-4">
                                     <input type="email" class="form-control" id="inputEmail3" placeholder="Ingrese su correo">
                                 </div>
                             </div>
                             <div class="form-group row">
                                 <label for="inputEmail3" class="col-sm-2 col-form-label">Usuario</label>
-                                <div class="col-sm-10">
+                                <div class="col-sm-4">
                                     <input type="text" class="form-control" id="inputEmail3" placeholder="Nombre de usuario">
                                 </div>
                             </div>
                             <div class="form-group row">
                                 <label for="inputEmail3" class="col-sm-2 col-form-label">Clave</label>
-                                <div class="col-sm-10">
+                                <div class="col-sm-4">
                                     <input type="text" class="form-control" id="inputEmail3" placeholder="Ingrese su clave">
                                 </div>
                             </div>
                             <div class="form-group row">
                                 <label for="inputEmail3" class="col-sm-2 col-form-label">Confirmar Clave</label>
-                                <div class="col-sm-10">
+                                <div class="col-sm-4">
                                     <input type="text" class="form-control" id="inputEmail3" placeholder="Reingrese su clave">
                                 </div>
                             </div>
